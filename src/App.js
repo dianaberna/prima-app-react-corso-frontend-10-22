f25895878452b8971d6e9565e4b76ac448ab2815
dianaberna <bernabei.d@gmail.com>
2022-11-22 17:58:39 +0100

    aggiornato la repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+// function component / componente funzionale
 function App() {
+  let numero = 10
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ciao 10+10 {numero}
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+function Button() {
+    return <p> gatto </p>;
+}
+
+export default Button;
