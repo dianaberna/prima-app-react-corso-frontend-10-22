32fcbfa8f5d03c4cf3d3693d16594d7222da3263
dianaberna <bernabei.d@gmail.com>
2022-11-23 10:28:19 +0100

    aggiornata repo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import "./App.css";
 
 // function component / componente funzionale
 function App() {
-  let numero = 10
-  return (
-    <div className="App">
-      ciao 10+10 {numero}
-    </div>
-  );
+    let numero = 10;
+
+    // ` ${}`
+    return <div className="App">ciao {numero + 10}</div>;
 }
 
 export default App;
+
+// funzione
+function stampaCiao() {
+    console.log("ciao");
+}
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <h1>Titolo pagina</h1>
+        <App colore="blue" />
+        <App> prova </App>
     </React.StrictMode>
 );
