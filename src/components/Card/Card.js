32df1b2f170b77dbfd02b2503a9e2cbbbe52dc23
dianaberna <bernabei.d@gmail.com>
2022-11-23 12:53:44 +0100

    aggiornato componente Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,12 @@
 import "./style.css"
+import imgcaffe from "./caffe.jpeg"
 
 function Card(){
     return(
         <div className="card">
             <div className="card-body">
-                {/* <img src="caffe.jpg" alt="caffe"> */}
+                <img src={imgcaffe} alt="caffe" />
+                <img src="./assets/caffepublic.jpeg" alt="caffe" />
                 <div>
                     <h1>Vanilla Latte</h1>
                     <p>21K</p>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Layout from "./components/Layout/Layout";
+import logo from "./logo.svg"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <h1>Titolo pagina</h1>
         <Layout />
+        <img src={logo} />
     </React.StrictMode>
 );
